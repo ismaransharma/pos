1f8708302498412b91dc3435241b568db6244ee0
ismaransharma <ismaransharma.14@gmail.com>
2025-05-28 11:03:21 +0545

    Barcode all done

--- barcode.js
+++ barcode.js
@@ -5,8 +5,6 @@ window.onload = function () {
 // Optional: Handle Enter key after scanning (common in scanners)
 document.getElementById("barcode").addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
-    e.preventDefault(); // Stop form submission if inside a form
-    console.log("Barcode scanned:", this.value);
-    // You can trigger form submission or additional logic here
+    e.preventDefault();
   }
 });
